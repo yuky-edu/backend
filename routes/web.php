1d89a606470f603167d98897f9b32ba58d4302c5
ARSAN <arsandev@users.noreply.github.com>
2021-03-22 20:56:08 +0700

    test push

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/debug', function () {
-    dd(bcrypt("host"));
+    return "debug";
 });
 Route::get('/', function () {
     return view('welcome');
